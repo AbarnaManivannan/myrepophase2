04220c11986370bbf465f3cbbc1bd244e1d87cf6
AbarnaManivannan <madhu.vravi@yahoo.com>
2025-04-21 00:04:24 +0530

    [ci-skip]Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "Setting permissions"
+
+# Give ownership to ec2-user
+chown ec2-user:ec2-user /home/ec2-user/app/app.jar
+
+# Make it executable
 chmod +x /home/ec2-user/app/app.jar
